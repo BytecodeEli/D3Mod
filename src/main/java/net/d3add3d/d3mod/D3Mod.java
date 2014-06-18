17163b585bd20c6916569710cbfa52926ba6b7b8
D3add3d <d3add3d@gmail.com>
2014-06-18 17:52:57 +0200

    Good old zombie smelting, can you smell it? :3

--- src/main/java/net/d3add3d/d3mod/D3Mod.java
+++ src/main/java/net/d3add3d/d3mod/D3Mod.java
@@ -2,11 +2,16 @@ package net.d3add3d.d3mod;
 
 import net.d3add3d.d3mod.lib.*;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraftforge.event.*;
 
 @Mod(modid = Reference.MODID, version = Reference.VERSION)
 public class D3Mod
@@ -16,6 +21,7 @@ public class D3Mod
     public void preInit(FMLPreInitializationEvent event)
     {
     	System.out.println("*********D3MOD PREINITIALIZING*********");
+    	
     }
     
     @EventHandler
@@ -28,6 +34,7 @@ public class D3Mod
     public void postInit(FMLPostInitializationEvent event)
     {
     	System.out.println("*********D3MOD POSTINITIALIZING*********");
+    	GameRegistry.addSmelting(Items.rotten_flesh, new ItemStack(Items.leather, 0), 1.0F);
     }
     
 }
